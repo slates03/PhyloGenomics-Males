d91c13619d6b3e2e3da754cb733e4a325dfe7ef1
slates03 <garett.p.slater@gmail.com>
2021-11-15 08:55:38 -0500

    Update json_Extraction_dnds.R

--- json_Extraction_dnds.R
+++ json_Extraction_dnds.R
@@ -4,17 +4,16 @@ module load r
 R
 
 
-dnds<-matrix(nrow=0,ncol=16)
-colnames(dnds)<-c("protein_id","AMEL_dN","AMEL_dS","AMEL_dNdS","BIMP_dN","BIMP_dS","BIMP_dNdS","MROT_dN","MROT_dS","MROT_dNdS","TCARB_dN","TCARB_dS",
-"TCARB_dNdS","EDILL_dN","EDIL_dS","EDIL_dNdS")
+dnds<-matrix(nrow=0,ncol=5)
+colnames(dnds)<-colnames(x)<-c("protein_id","dN","dS","dNdS","Species")
 
-json<-read.table("/depot/bharpur/data/projects/slater/Phylo_MaleEvolution/Resources/Alignment/hyphy/dnds/json_4.txt")
+json<-read.table("/depot/bharpur/data/projects/slater/Phylo_MaleEvolution/Resources/Alignment/hyphy/dnds/json_3.txt")
 json<-as.character(json[,1])
 
 for(i in json){
   
   
-  attach<-"_aligned_4.fasta.FITTER.json"
+  attach<-"_aligned_3.fasta.FITTER.json"
   file<-gsub(" ", "", paste(i, attach))
   
   
@@ -41,32 +40,40 @@ for(i in json){
   TCARB_DN<-data.frame(my.JSON$`branch attributes`$`0`$TR_tetragonula_carbonaria$dN[[1]])
 
   
-  x<-data.frame(matrix(nrow=1,ncol=16))
+  x<-data.frame(matrix(nrow=1,ncol=5))
   
-  x[,1]<-i
-  x[,2]<-AMEL_DN
-  x[,3]<-AMEL_DS
-  x[,4]<-(as.numeric(x[,2]))/(as.numeric(x[,3]))
-  x[,5]<-BIMP_DN
-  x[,6]<-BIMP_DN
-  x[,7]<-(as.numeric(x[,5]))/(as.numeric(x[,6]))
-  x[,8]<-MROT_DN
-  x[,9]<-MROT_DS
-  x[,10]<-(as.numeric(x[,8]))/(as.numeric(x[,9]))
-  x[,11]<-TCARB_DN
-  x[,12]<-TCARB_DS
-  x[,13]<-(as.numeric(x[,11]))/(as.numeric(x[,12]))
-  x[,14]<-EDIL_DN
-  x[,15]<-EDIL_DS
-  x[,16]<-(as.numeric(x[,14]))/(as.numeric(x[,15]))
+  x[1,1]<-i
+  x[1,2]<-AMEL_DN
+  x[1,3]<-AMEL_DS
+  x[1,4]<-(as.numeric(x[1,2]))/(as.numeric(x[1,3]))
+  x[1,5]<-"AMEL"
+  x[2,1]<-i
+  x[2,2]<-BIMP_DN
+  x[2,3]<-BIMP_DS
+  x[2,4]<-(as.numeric(x[2,2]))/(as.numeric(x[2,3]))
+  x[2,5]<-"BIMP"
+  x[3,1]<-i
+  x[3,2]<-MROT_DN
+  x[3,3]<-MROT_DS
+  x[3,4]<-(as.numeric(x[3,2]))/(as.numeric(x[3,3]))
+  x[3,5]<-"MROT"
+  x[4,1]<-i
+  x[4,2]<-TCARB_DN
+  x[4,3]<-TCARB_DS
+  x[4,4]<-(as.numeric(x[4,2]))/(as.numeric(x[4,3]))
+  x[4,5]<-"TCARB"
+  x[5,1]<-i
+  x[5,2]<-EDIL_DN
+  x[5,3]<-EDIL_DS
+  x[5,4]<-(as.numeric(x[5,2]))/(as.numeric(x[5,3]))
+  x[5,5]<-"EDIL"
   
-  colnames(x)<-c("protein_id","AMEL_dN","AMEL_dS","AMEL_dNdS","BIMP_dN","BIMP_dS","BIMP_dNdS","MROT_dN","MROT_dS","MROT_dNdS","TCARB_dN","TCARB_dS",
-                     "TCARB_dNdS","EDILL_dN","EDIL_dS","EDIL_dNdS")
+  colnames(x)<-c("protein_id","dN","dS","dNdS","Species")
   
   dnds<-rbind(dnds,x)
   
   
 }
 
-write.table(dnds,"/depot/bharpur/data/projects/slater/Phylo_MaleEvolution/Resources/Alignment/hyphy/dnds/dnds_4.txt")
+write.table(dnds,"/depot/bharpur/data/projects/slater/Phylo_MaleEvolution/Resources/Alignment/hyphy/dnds/dnds_3.txt")
 
